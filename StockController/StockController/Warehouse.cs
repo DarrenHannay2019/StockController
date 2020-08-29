09ae56531f7230ec446ae930ca56725a52207899
DarrenHannay2019 <dmhsoftad01@outlook.com>
2020-08-29 17:36:39 +0100

    Warehouse Function added No code on form. start coding the object class warehouse

---
+++ StockController/StockController/Warehouse.cs
@@ -0,0 +1,25 @@
+﻿namespace StockController
+{
+    using System;
+    using System.Data;
+    using System.Data.SqlClient;
+ 
+    public class Warehouse : Utilities
+    {
+        public string WarehouseName { get; set; }
+        public string WarehouseType { get; set; }
+        public Warehouse()
+        {
+            SaveToDB = false;
+            UpdateToDB = false;
+            DeleteFromDB = false;
+        }
+        ~Warehouse()
+        {
+            SaveToDB = false;
+            UpdateToDB = false;
+            DeleteFromDB = false;
+        }
+
+    }
+}
